2780d2f52d86c2bdcfbf973a12e9e9d94b26ebab
dongkunshan <windwithfo@yeah.net>
2022-03-02 18:04:06 +0800

    use pnpm

--- bin/wc-init.mjs
+++ bin/wc-init.mjs
@@ -50,7 +50,7 @@ checkTemp().then(function() {
     type: 'list',
     name: 'pkg',
     message: 'select a template',
-    choices: ['yarn', 'npm']
+    choices: ['pnpm', 'yarn', 'npm']
   }]).then((answers) => {
     // 判断用户输入，调用项目初始化方法
     copyTemp(path.join(config.temp.dir, answers.type), answers.name, answers.pkg)
